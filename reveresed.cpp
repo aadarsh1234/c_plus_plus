b2789f4d8c5e2ee20d54430e7f7e7169ac24f73e
Aadarshdubey637 <dubeyaadarsh637@gmail.com>
2025-03-27 15:44:01 +0530

    Update reveresed.cpp

--- reveresed.cpp
+++ reveresed.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    int num,reversed=0;
+    std::cout<<"Enter a number";
+    cin>>num;
+    while(num!=0){
+        int digit=num % 10;
+        reversed =reversed*10+digit;
+        num/=10;
+    }
+    cout<<"Rerversed number :"<<reversed<<endl;
+    return 0;
+}
