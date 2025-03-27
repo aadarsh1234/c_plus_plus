5b23165329c395a2622fe71ed29607289525e464
Aadarshdubey637 <dubeyaadarsh637@gmail.com>
2025-03-27 15:45:37 +0530

    Update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -1,1 +1,16 @@
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num,sum=0;
+    std::cout<<"Enter a number";
+    cin>>num;
+    while(num!=0){
+        sum+=num%10;
+        num/=10;
+    }
+    cout<<"sum of digit"<<sum<<endl;
+    return 0;
+}
