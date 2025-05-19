3e26fee116b0ea5f5edc53e52c30b96a18dcf129
Aadarsh-dubey <dubeyaadarsh637@gmail.com>
2025-05-19 23:30:40 +0530

    Create prime-class,obj.cpp

---
+++ prime-class,obj.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+using namespace std;
+class Prime{
+    int n;
+    public:
+    void Input(){
+        cout<<"Enter any number:";
+        cin>>n;
+    }
+    void Output(){
+        int count=0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                count++;
+            }
+        }
+        if(count==2)
+        cout<<"Prime number";
+        else
+        cout<<"Not prime number";
+    }
+};
+
+int main()
+{
+    Prime obj;
+    obj.Input();
+    obj.Output();
+    return 0;
+}
