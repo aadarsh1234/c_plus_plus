07c0091c63be03be16e7bbe6ea080f614fcabb62
Aadarsh-dubey <dubeyaadarsh637@gmail.com>
2025-06-24 23:28:28 +0530

    Update Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -64,3 +64,20 @@ int main()
         
     return 0;
 }
+// add two array
+
+#include <iostream>
+using namespace std;
+int main()
+{
+int a[4]={1,2,3,4};
+int b[4]={1,2,3,4};
+int c[4];
+for (int i=0;i<4;i++){
+    c[i]=a[i]+b[i];
+}
+for(int i=0;i<4;i++){
+    cout<<c[i]<<" ";
+}
+    return 0;
+}
